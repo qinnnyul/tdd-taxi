8b25609d4b130126ca7049734ce38a841c7f8998
Yulin Qin <yulin.qin.ext@gmail.com>
2018-11-07 21:52:59 +0800

    small change

--- src/main/java/com/qinnnyul/taxi/config/TaxiConfig.java
+++ src/main/java/com/qinnnyul/taxi/config/TaxiConfig.java
@@ -5,16 +5,16 @@ import java.util.Properties;
 
 public class TaxiConfig {
 
-    private String resouceName;
+    private String resourceName;
 
-    public TaxiConfig(String resouceName) {
-        this.resouceName = resouceName;
+    public TaxiConfig(String resourceName) {
+        this.resourceName = resourceName;
     }
 
     public String load(String taxiType, String time, String key) throws IOException {
         Properties properties = new Properties();
 
-        properties.load(this.getClass().getResourceAsStream(resouceName));
+        properties.load(this.getClass().getResourceAsStream(resourceName));
 
         return properties.getProperty(String.format("%s.%s.%s", taxiType, time, key));
     }
